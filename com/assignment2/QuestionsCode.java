660774f23d4b65c04d1a5b589b3eb1b5c5a01e7b
James Griffin <Jim@Jamess-MacBook-Pro-2.local>
2019-03-12 22:51:58 -0500

    First commit

---
+++ com/assignment2/QuestionsCode.java
@@ -0,0 +1,13 @@
+package com.assignment2;
+
+public class QuestionsCode 
+{
+	static int a=0; 
+	int b=++a; 
+	static int c=++a; 
+		
+	public static void main(String[] args)	
+	{ 
+		System.out.print(c); 
+	} 
+}
